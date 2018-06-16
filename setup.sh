63442fa956be937f273654aab7b4f4fc495f9ccb
Anton Wildtraut <anton.wildtraut@telekom.de>
2018-06-17 01:25:02 +0200

    simplified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,42 +1,4 @@
 #!/usr/bin/env bash
-
-ask() {
-    # https://djm.me/ask
-    local prompt default reply
-
-    while true; do
-
-        if [ "${2:-}" = "Y" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "N" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
-
-        # Ask the question (not using "read -p" as it uses stderr not stdout)
-        echo -n "$1 [$prompt] "
-
-        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
-        read reply </dev/tty
-
-        # Default?
-        if [ -z "$reply" ]; then
-            reply=$default
-        fi
-
-        # Check if the reply is valid
-        case "$reply" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
-        esac
-
-    done
-}
-
 download_weights() {
     echo "=> Downloading weights (140BnF)"
     mkdir weights
@@ -51,28 +13,16 @@ download_cfg() {
 }
 
 if [ -d "./weights/" ]; then
-    if ask "++ Folder ./weights/ found! Delete it?" Y; then
-        rm -rf ./weights/
-        download_weights
-    fi
-else
-    download_weights
+    rm -rf ./weights/ 
 fi
+download_weights
 
-echo
 if [ -d "./cfg/" ]; then
-    if ask "++ Folder ./cfg/ found! Delete it?" Y; then
-        rm -rf ./cfg/
-        download_cfg
-    fi
-else
-    download_cfg
+    rm -rf ./cfg/
 fi
+download_cfg
 
-echo
-if ask "++ Would you like to build darknet?" Y; then
-    echo "=> Building darknet\(TM\)"
-    cd darknet
-    python3 setup.py build_ext --inplace
-    cd ..
-fi
+echo "=> Building darknet\(TM\)"
+cd darknet
+python3 setup.py build_ext --inplace
+cd ..
