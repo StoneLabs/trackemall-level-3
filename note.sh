319808949df92c2d47f57fd0b0a67c498624296e
StoneInc <levyehrstein@googlemail.com>
2018-06-17 05:17:16 +0200

    Professional error prevention methods

--- note.sh
+++ note.sh
@@ -1,1 +1,3 @@
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/darknet/ python3.6 note.py
+echo Starting infinite loop in T-3
+sleep 3
+while true; do LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/darknet/ python3.6 note.py; done
