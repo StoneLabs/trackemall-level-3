6a8697846ebaa6d4e53b555ce38df3eb465f9654
StoneInc <levyehrstein@googlemail.com>
2018-06-17 10:18:37 +0200

    Working software

--- note.sh
+++ note.sh
@@ -1,3 +1,3 @@
 echo Starting infinite loop in T-3
 sleep 3
-while true; do LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/darknet/ python3.6 note.py; done
+while true; do LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/darknet/ python3.6 note.py; sleep 5; done
